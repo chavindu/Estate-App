cae239ca76720616ca7642524316d61bc232cd9c
Chavindu Nuwanpriya <chavindun@gmail.com>
2021-02-26 21:03:04 +0530

    Added Login UI

---
+++ src/EstateApp.Web/Controllers/AccountsController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstateApp.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+         public IActionResult Login()
+        {
+            return View();
+        }
+
+    
+         public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
